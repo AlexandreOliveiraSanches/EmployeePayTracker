d2a4ab406fb031213dd0dd901b0529d3d18aa38f
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-08-11 10:44:21 -0300

    List of Data for Own and Outsourced Employees

---
+++ EmployeePayTracker/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using EmployeePayTracker.Entities;
+using System.Globalization;
+
+namespace EmployeePayTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char resp = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: $");
+                double value = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (resp == 'y' || resp == 'Y')
+                {
+                    Console.Write("Additional charge: $");
+                    double additional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, value, additional));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, value));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
